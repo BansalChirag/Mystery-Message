a535f8f2f17370180ce15c4a16685f368c2ec0e0
Chirag Bansal <78252773+BansalChirag@users.noreply.github.com>
2024-07-20 15:39:42 +0530

    Added suspense boundary for the use of searchParams

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,12 +1,12 @@
-
-import ForgotPasswordForm from '@/components/forms/forgot-password-form'
 import ResetPasswordForm from '@/components/forms/reset-password-form'
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const page = () => {
   return (
     // <div className='flex items-center justify-center h-screen p-4'>
-        <ResetPasswordForm/>
+    <Suspense>
+      <ResetPasswordForm/>
+    </Suspense>
     // </div>
   )
 }
