12751b832ba0520fbbbc9056ed12ea4ce97ecf30
Chirag Bansal <mylocal@test.com>
2025-02-04 22:53:40 +0530

    modified the logic of checking the username

--- app/api/check-username-unique/route.ts
+++ app/api/check-username-unique/route.ts
@@ -10,23 +10,23 @@ const suggestUsernames = async (username: string) => {
     const takenUsernames = users?.map((user) => user.username);
     // if(takenUsernames.length>0){
 
-      const prompt = `Suggest 5 strong and available usernames that start with ${username} and follow the regex /^(?=[a-zA-Z_]+[a-zA-Z0-9_.]*)(?=[a-zA-Z0-9_]+)[a-zA-Z0-9_\.]{3,30}(?<!\.)(?!\.{2,})$/. 
+    const prompt = `Suggest 5 strong and available usernames that start with ${username} and follow the regex /^(?=[a-zA-Z_]+[a-zA-Z0-9_.]*)(?=[a-zA-Z0-9_]+)[a-zA-Z0-9_\.]{3,30}(?<!\.)(?!\.{2,})$/. 
       Make the suggestions similar to Instagram usernames, with a mix of words, numbers, and underscores. 
       Avoid suggesting usernames that are already taken, including ${takenUsernames?.join(
         ", "
       )}. Prioritize suggestions that are easy to remember and have a good balance of characters.
       return it without adding any list styling just like that simple username list 
       `;
-  
-      const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
-      const model = genAI.getGenerativeModel({
-        model: "gemini-pro",
-      });
-  
-      const result = await model.generateContentStream(prompt);
-      const response = await result.response;
-      const text = response.text();
-      return text.split("\n").map((username) => username.trim());
+
+    const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY!);
+    const model = genAI.getGenerativeModel({
+      model: "gemini-pro",
+    });
+
+    const result = await model.generateContentStream(prompt);
+    const response = await result.response;
+    const text = response.text();
+    return text.split("\n").map((username) => username.trim());
     // }
     // return [];
   } catch (error) {
@@ -53,19 +53,26 @@ export async function GET(request: NextRequest) {
   }
 
   const username = queryParams.username;
+  const resault = await User.findOne({ email: "limal97924@kuandika.com" });
 
-  const users = await User.find({ verifyCodeExpiry: { $lt: Date.now() } }, { username: 1 });
-  const existingVerifiedUser = users?.find(
-    (user) => user.username === username
-  );
+  // const users = await User.find(
+  //   { verifyCodeExpiry: { $lt: Date.now() } },
+  //   { username: 1 }
+  // );
+  const user = await User.findOne({
+    username: username,
+    $or: [{ verifyCodeExpiry: { $lt: Date.now() } }, { isVerified: true }],
+  });
+  // const existingVerifiedUser = users?.find(
+  //   (user) => user.username === username
+  // );
+  // console.log("🚀 ~ GET ~ existingVerifiedUser:", existingVerifiedUser);
 
   // it will be in the form of
   // existingVerifiedUser: { _id: new ObjectId(''), username: 'some username' }
 
-  if (existingVerifiedUser?.username) {
-    const suggestedUsernames = await suggestUsernames(
-      existingVerifiedUser?.username
-    );
+  if (user) {
+    const suggestedUsernames = await suggestUsernames(user?.username);
     return NextResponse.json(
       {
         success: false,
